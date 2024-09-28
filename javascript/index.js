7e667867c8029b26ba8d2f72868d866b010e0d41
Cassy <cassandra.geroulis@gmail.com>
2024-09-28 14:50:14 +1000

    Changed from poems to jokes

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,26 @@
-function displayPoem(response) {
-    new Typewriter("#poem", {
+function displayJoke(response) {
+    new Typewriter("#joke", {
         strings: response.data.answer,
         autoStart: true,
-        delay: 50, 
+        delay: 50,
         cursor: "",
     });
 }
 
-function generatePoem(event) {
+function generateJoke(event) {
     event.preventDefault();
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "0cat4ed5a0453d5608fc6b4od86b35fc";
-    let context = "You are a poem expert and love to write short poems. Your mission is to generate a 4-line poem and separate each line with a <br />. Make sure to follow the user instructions.";
-    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    let context = "You are a joke expert and love to create funny jokes. Your mission is to generate a short, humorous joke based on the user's instructions.";
+    let prompt = `User instructions: Generate a joke about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    let poemElement = document.querySelector("#poem");
-    poemElement.classList.remove("hidden");
+    let jokeElement = document.querySelector("#joke");
+    jokeElement.classList.remove("hidden");
 
-    axios.get(apiURL).then(displayPoem);
+    axios.get(apiURL).then(displayJoke);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let jokeFormElement = document.querySelector("#joke-generator-form");
+jokeFormElement.addEventListener("submit", generateJoke);
