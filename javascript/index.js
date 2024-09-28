334e804052cfff675d0ad6c896dfa60fc969bb40
Cassy <cassandra.geroulis@gmail.com>
2024-09-28 12:47:55 +1000

    Added basic form handling and added typewriter plugin

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,16 @@
-function showAnswer(response) {
-    
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+        strings: "As a quiet little seedling",
+        autoStart: true,
+        delay: 20,
+        cursor: "",
+    });
 }
 
-let apiKey = "0cat4ed5a0453d5608fc6b4od86b35fc"; 
-let context = "be polite and provide a very short answer";
-let prompt = "can you tell me a joke?";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
 
-axios.get(apiUrl)
-    .then(showAnswer)
+
 
